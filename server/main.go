cdb9e6e3e10634a5b0c423a60f8c9937411442ca
Matteo Marchelli <matteo.marchelli@gmail.com>
2025-04-01 14:16:18 +0100

    adding logging to serve (needs something better)

--- server/main.go
+++ server/main.go
@@ -2,16 +2,43 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path"
 	"time"
 )
+
+type dirpath string
+
+func (p dirpath) serveFile(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	fullpath := path.Join(string(p), r.RequestURI)
+	status := http.StatusOK
+	_, err := os.Stat(fullpath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			status = http.StatusNotFound
+			log.Printf("%3d % 10s %s \n", status, time.Since(start).Round(time.Nanosecond), fullpath)
+			http.NotFound(w, r)
+			return
+		}
+		status = http.StatusInternalServerError
+		fmt.Fprint(w, "Internal Server Error")
+		log.Printf("%3d % 10s %s \n", status, time.Since(start).Round(time.Nanosecond), fullpath)
+		return
+	}
+	http.ServeFile(w, r, fullpath)
+	log.Printf("%3d % 10s %s \n", status, time.Since(start).Round(time.Nanosecond), fullpath)
+}
 
 func main() {
 	var dir, addr string
 	flag.StringVar(&dir, "dir", "../dist", "dir to serve")
 	flag.StringVar(&addr, "addr", "0.0.0.0:8080", "addr to bind")
 	flag.Parse()
+	p := dirpath(dir)
 
 	s := &http.Server{
 		Addr:           addr,
@@ -21,7 +48,7 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	http.Handle("/", http.FileServer(http.Dir(dir)))
+	http.HandleFunc("/", p.serveFile)
 
 	log.Printf("Serving files from %s on %s\n", dir, addr)
 	log.Fatal(s.ListenAndServe())
